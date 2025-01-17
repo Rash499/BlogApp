27fa69f197169cd920477589e3a41613c0a973de
Rash499 <rashmikadilmin499@gmail.com>
2025-01-17 23:51:09 +0530

    search, postmenuactions created, single postpage done

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from '../components/Image'
 import { Link } from 'react-router-dom'
+import PostMenuAction from '../components/PostMenuActions'
+import Search from '../components/Search'
 
 const SinglePostPage = () => {
   return (
@@ -35,9 +37,72 @@ const SinglePostPage = () => {
         </div>
       </div>
        {/*content*/}
-       <div className='flex flex-col md:flex-row gap-8'>
+       <div className='flex flex-col md:flex-row gap-12'>
           {/*text*/}
           <div className='lg:text-lg flex flex-col gap-6 text-justify'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
+              eaque recusandae id quam possimus fuga iste aut nemo sapiente provident pariatur.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum expedita quis ea. 
+              Expedita quas nemo, eius quae officia at eveniet. Velit,
+              culpa doloribus quam asperiores minima aliquid accusamus quidem molestiae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
+              eaque recusandae id quam possimus fuga iste aut nemo sapiente provident pariatur.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum expedita quis ea. 
+              Expedita quas nemo, eius quae officia at eveniet. Velit,
+              culpa doloribus quam asperiores minima aliquid accusamus quidem molestiae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
+              eaque recusandae id quam possimus fuga iste aut nemo sapiente provident pariatur.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum expedita quis ea. 
+              Expedita quas nemo, eius quae officia at eveniet. Velit,
+              culpa doloribus quam asperiores minima aliquid accusamus quidem molestiae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
+              eaque recusandae id quam possimus fuga iste aut nemo sapiente provident pariatur.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum expedita quis ea. 
+              Expedita quas nemo, eius quae officia at eveniet. Velit,
+              culpa doloribus quam asperiores minima aliquid accusamus quidem molestiae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
+              eaque recusandae id quam possimus fuga iste aut nemo sapiente provident pariatur.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum expedita quis ea. 
+              Expedita quas nemo, eius quae officia at eveniet. Velit,
+              culpa doloribus quam asperiores minima aliquid accusamus quidem molestiae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
+              eaque recusandae id quam possimus fuga iste aut nemo sapiente provident pariatur.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum expedita quis ea. 
+              Expedita quas nemo, eius quae officia at eveniet. Velit,
+              culpa doloribus quam asperiores minima aliquid accusamus quidem molestiae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
+              eaque recusandae id quam possimus fuga iste aut nemo sapiente provident pariatur.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum expedita quis ea. 
+              Expedita quas nemo, eius quae officia at eveniet. Velit,
+              culpa doloribus quam asperiores minima aliquid accusamus quidem molestiae.
+            </p>
+
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
               At nam maxime dicta ut. Nostrum vero quaerat veniam reiciendis, 
@@ -67,27 +132,47 @@ const SinglePostPage = () => {
           </div>
           {/*menu*/}
           <div className='px-4 h-max sticky top-8'>
-            <h1>Author</h1>
-            <div className=''>
-              <Image 
-                src='userImg.jpeg' 
-                className='w-12 h-12 rounded-full object-cover' 
-                w='48' 
-                h='48' 
-              />
+            <h1 className='mb-4 text-sm font-medium'>Author</h1>
+            <div className='flex flex-col gap-4'>
+              <div className='flex items-center gap-8'>
+                <Image 
+                  src='userImg.jpeg' 
+                  className='w-12 h-12 rounded-full object-cover' 
+                  w='48' 
+                  h='48' 
+                />
 
-              <Link>John Doe</Link>
-              <p>Lorem ipsum dolor sit.</p>
-              <div className='flex gap-2'>
-                <Link>
-                  <Image src='facebook.svg' />
-                </Link>
+                <Link className='text-blue-800'>John Doe</Link>
+                </div>
+                  <p className='text-sm text-gray-500'>Lorem ipsum dolor sit.</p>
+                  <div className='flex gap-2'>
+                    <Link>
+                      <Image src='facebook.svg' />
+                    </Link>
 
-                <Link>
-                  <Image src='instagram.svg' />
-                </Link>
-              </div>
+                    <Link>
+                      <Image src='instagram.svg' />
+                    </Link>
+                </div>
             </div>
+            <PostMenuAction />
+            <h1 className='mt-8 mb-4 text-sm font-medium'>Categories</h1>
+            <div className='flex flex-col gap-2 test-sm'>
+              <Link className='underline'>All</Link>
+
+              <Link className='underline' to='/'>Web Design</Link>
+
+              <Link className='underline' to='/'>Development</Link>
+
+              <Link className='underline' to='/'>Database</Link>
+
+              <Link className='underline' to='/'>Search Engine</Link>
+
+              <Link className='underline' to='/'>Marketing</Link>
+            </div>
+
+            <h1 className='mt-8 mb-4 text-sm font-medium'>Search</h1>
+            <Search />
           </div>
        </div>
     </div>
