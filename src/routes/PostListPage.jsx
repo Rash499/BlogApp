5fb44c4dffca9af72b393ebcba2238b19dd19e38
Rash499 <rashmikadilmin499@gmail.com>
2025-01-20 19:18:56 +0530

    postlist component

--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import PostList from '../components/PostList'
+import SideMenu from '../components/SideMenu'
 const PostListPage = () => {
+
+  const [open,setOpen] = useState(false);
   return (
-    <div>PostListPage</div>
+    <div className=''>
+      <h1 className='mb-8 text-2xl'>Development</h1>
+      <button onClick={() => setOpen((prev) => !prev)} className='bg-blue-800 text-sm text-white px-2 py-2 rounded-2xl mb-4 md:hidden'>
+        {open ? "Close" : "Filter or Search"}
+      </button>
+      <div className='flex flex-col gap-8'>
+        <div className=''>
+          <PostList />
+        </div>
+        <div className=''>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
